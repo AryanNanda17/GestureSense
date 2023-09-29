efa7adce49a60b3ff0c8127e424d650453d368e1
Aryan <nandaaryan823@gmail.com>
2023-09-29 15:37:41 +0530

    Hand Detection file Added
    Hand detection updated
    
    Hand detection updated
    
    Hand detection updated
    
    minor change
    
    minor change

---
+++ Hand Detectio Using OpenCV/HSV_Centroid.py
@@ -0,0 +1,56 @@
+import cv2 as cv
+import numpy as np
+
+capture = cv.VideoCapture(1)
+
+# Center crop for 720 * 1280 screen
+height, width = 720, 1280
+crop_height, crop_width = 650, 800 
+
+# coordinates of the top-left corner of the crop
+x1 = (width - crop_width) // 2
+y1 = (height - crop_height) // 2
+
+while True:
+
+    isTrue, frame = capture.read()
+    cv.rectangle(frame, (x1, y1), (x1 + crop_width, y1 + crop_height), (0, 0, 255), 2)
+    # roi = frame[160:560, 440:840]
+    roi = frame[y1:y1+crop_height, x1:x1+crop_width]
+    hsv = cv.cvtColor(roi, cv.COLOR_BGR2HSV)
+
+    lower_range = np.array([0, 20, 70], dtype=np.uint8)
+    upper_range = np.array([20, 255, 255], dtype=np.uint8)
+
+    mask = cv.inRange(hsv, lower_range, upper_range)
+
+    contours, hierarchy = cv.findContours(mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+
+    # Finding largest contour
+    largest_contour = 0
+    max_area = 1e-16
+    for contour in contours:
+        area = cv.contourArea(contour)
+        if area > max_area:
+            max_area = area
+            largest_contour = contour
+
+    if max_area > 100 :
+        x, y, w, h = cv.boundingRect(largest_contour)
+        cv.rectangle(roi, (x, y), (x + w, y + h), (255, 0, 255), 3)
+
+        # Finding Centroid of hand
+        Centroid = cv.moments(largest_contour)
+        if (Centroid["m00"] != 0):
+            cv.circle(roi, ( int(Centroid["m10"] / Centroid["m00"] + 1e-5), int(Centroid["m01"] / Centroid["m00"] + 1e-5) ), 5, (0, 0, 255), -1)
+
+    cv.imshow('Hand Detection', frame)
+
+    if cv.waitKey(20) & 0xFF == ord('d'):
+        break
+
+capture.release()
+cv.destroyAllWindows()
+
+
+
